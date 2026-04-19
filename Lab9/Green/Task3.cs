b54806ec623556bfb29feca5627dad725ec25379
Nghia244 <m2504458@edu.misis.ru>
2026-04-19 20:50:50 +0300

    Create Task3.cs

---
+++ Lab9/Green/Task3.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Lab9
+{
+    public class Mark
+    {
+        public string StudentId { get; set; }
+        public string SubjectId { get; set; }
+        public double Score { get; set; }
+
+        public Mark() { }
+
+        public Mark(string studentId, string subjectId, double score)
+        {
+            StudentId = studentId;
+            SubjectId = subjectId;
+            Score = score;
+        }
+
+        public void DisplayMark()
+        {
+            Console.WriteLine($"Student ID: {StudentId} | Subject ID: {SubjectId} | Score: {Score}");
+        }
+    }
+}
