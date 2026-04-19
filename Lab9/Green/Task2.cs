fa22d649e7bb2cac90b963875a477f813498ab4d
Nghia244 <m2504458@edu.misis.ru>
2026-04-19 20:50:23 +0300

    Create Task2.cs

---
+++ Lab9/Green/Task2.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Lab9
+{
+    public class Subject
+    {
+        public string SubjectId { get; set; }
+        public string SubjectName { get; set; }
+        public int Credit { get; set; }
+
+        public Subject() { }
+
+        public Subject(string id, string name, int credit)
+        {
+            SubjectId = id;
+            SubjectName = name;
+            Credit = credit;
+        }
+
+        public void DisplaySubject()
+        {
+            Console.WriteLine($"ID: {SubjectId} | Name: {SubjectName} | Credits: {Credit}");
+        }
+    }
+}
